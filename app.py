c7f33284de236e8cc13fbcefc1278a2cdc727ec7
Programmingjoe <joeczepil@gmail.com>
2017-09-10 13:50:22 -0700

    Internationalization basic implementation

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask.ext.babel import Babel
+from flask import render_template
+from flask import g, request
+from flask.ext.babel import gettext
+
 app = Flask(__name__)
 
-from flask import render_template
+app.config.from_pyfile('config.py')
+babel = Babel(app)
 
 @app.route('/')
 def index():
@@ -18,3 +24,7 @@ def about():
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
