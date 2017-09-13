0b8a29b21e04007fbf697d92cc88e15c70acc29a
Programmingjoe <joeczepil@gmail.com>
2017-09-12 20:13:58 -0700

    Styling and pricing to donate

--- static_site/views.py
+++ static_site/views.py
@@ -8,8 +8,8 @@ def index():
   return render_template('index.html')
 
 @app.route('/pricing')
-def pricing():
-  return render_template('pricing.html')
+def donate():
+  return render_template('donate.html')
 
 @app.route('/about')
 def about():
