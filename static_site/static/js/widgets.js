f2cf81d11f2b66d00eb00fcfafa5abde03452759
Programmingjoe <joeczepil@gmail.com>
2017-09-28 22:05:20 -0700

    Responsive improvements, hamburger menu

---
+++ static_site/static/js/widgets.js
@@ -0,0 +1,31 @@
+function showHamburger() {
+  $('#hamburger-menu').animate({
+    left: '0vw'
+  });
+  $('#hamburger-x').animate({
+    opacity: '1'
+  });
+  $('#hamburger-lines').animate({
+    opacity: '0'
+  });
+  document.getElementById('hamburger-menu').style.display = 'block';
+  document.getElementById('hamburger-x').style.display = 'block';
+  document.getElementById('hamburger-lines').style.display = 'none';
+  $('body').addClass('body-no-scroll');
+}
+
+function hideHamburger() {
+  $('#hamburger-menu').animate({
+    left: '100vw'
+  });
+  $('#hamburger-x').animate({
+    opacity: '0'
+  });
+  $('#hamburger-lines').animate({
+    opacity: '1'
+  });
+  document.getElementById('hamburger-menu').style.display = 'none';
+  document.getElementById('hamburger-x').style.display = 'none';
+  document.getElementById('hamburger-lines').style.display = 'block';
+  $('body').removeClass('body-no-scroll');
+}
