c216c8c7d87a91d1e64d342762b23d9c2c5e69a4
Joe Czepil <joeczepil@gmail.com>
2017-10-10 20:56:08 -0700

    Styling changes, donate button added

--- static_site/static/js/widgets.js
+++ static_site/static/js/widgets.js
@@ -1,18 +1,22 @@
 function showHamburger() {
+  document.getElementById('hamburger-lines').style.display = 'none';
+  document.getElementById('hamburger-menu').style.display = 'block';
   $('#hamburger-menu').animate({
     left: '0vw'
   });
   $('#hamburger-x').animate({
     opacity: '1'
   });
-  $('#hamburger-lines').animate({
-    opacity: '0'
-  });
-  document.getElementById('hamburger-menu').style.display = 'block';
   $('body').addClass('body-no-scroll');
 }
 
 function hideHamburger() {
+  setTimeout(function(){
+  document.getElementById('hamburger-lines').style.display = 'block';
+  }, 350);
+  setTimeout(function(){
+    document.getElementById('hamburger-menu').style.display = 'none';
+  }, 350);
   $('#hamburger-menu').animate({
     left: '100vw'
   });
@@ -22,8 +26,5 @@ function hideHamburger() {
   $('#hamburger-lines').animate({
     opacity: '1'
   });
-  setTimeout(function(){
-    document.getElementById('hamburger-menu').style.display = 'none';
-  }, 1000);
   $('body').removeClass('body-no-scroll');
 }
