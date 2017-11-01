343d0f1a09af2406ca82a13617201ce067d8b7bc
Programmingjoe <joeczepil@gmail.com>
2017-11-01 09:01:47 -0700

    Update email pass

--- static_site/__init__.py
+++ static_site/__init__.py
@@ -14,7 +14,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'liefbaseinfo@gmail.com'
-app.config["MAIL_PASSWORD"] = 'Kraftpeanutbutter8'
+app.config["MAIL_PASSWORD"] = 'AdamPeanutbutter8'
 
 mail = Mail()
 mail.init_app(app)
