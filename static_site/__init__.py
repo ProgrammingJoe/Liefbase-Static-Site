3ab835e03b01cc428722c80cef2bc1415d679aeb
Programmingjoe <joeczepil@gmail.com>
2017-11-01 09:52:50 -0700

    Use liefbaseinfo email

--- static_site/__init__.py
+++ static_site/__init__.py
@@ -10,8 +10,14 @@ app.config.from_pyfile('config.py')
 
 app.secret_key = '\xc5\x10!1\x1c\xd1\xf7\x83yQ\x9b\x96\xdazx\x93\xf1\x94\x96c\x9c\x0b\x17\xb9'
 
-mail = Mail()
-mail.init_app(app)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USERNAME"] = 'liefbaseinfo@gmail.com'
+app.config["MAIL_PASSWORD"] = 'AdamPeanutbutter8'
+
+mail = Mail(app)
 
 
 import views
